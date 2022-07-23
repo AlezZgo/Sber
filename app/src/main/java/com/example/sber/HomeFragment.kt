721b102ab5a5047eff4cd3dfbb0bc82c7f9d59d4
vitya <sonyaka.2012@yandex.ru>
2022-07-24 01:18:17 +0300

    Hilt viewModel

--- app/src/main/java/com/example/sber/HomeFragment.kt
+++ app/src/main/java/com/example/sber/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.sber.databinding.FragmentHomeBinding
 import com.example.sber.di.MockClass
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,13 +17,15 @@ class HomeFragment : Fragment() {
     @Inject
     lateinit var mock: MockClass
 
+    private val vm : HomeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentHomeBinding.inflate(inflater,container,false)
 
-        binding.tv.text = mock.test()
+        binding.tv.text = vm.test()
 
         return binding.root
     }
